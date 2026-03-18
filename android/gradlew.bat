10177cefd26a4854b944dbc678328fab638118bf
unknown <kaustav.savemedha@gmail.com>
2026-03-18 16:10:33 +0530

    kabir 18/3

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -72,21 +72,18 @@ goto fail
 
 set CLASSPATH=
 
-pushd "%APP_HOME%" >NUL
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
-set EXIT_CODE=%ERRORLEVEL%
-popd >NUL
 
 :end
 @rem End local scope for the variables with windows NT shell
-if %EXIT_CODE% equ 0 goto mainEnd
+if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if not defined EXIT_CODE set EXIT_CODE=%ERRORLEVEL%
+set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
 if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
